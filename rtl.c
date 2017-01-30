8a787cbdb67f039a593d50c8707994f888fc5d00
Tomasz Lemiech <szpajder@gmail.com>
2017-01-30 21:24:18 +0100

    Set RTL gain to the closest value which is actually supported by the device
    rtlsdr_set_tuner_gain() fails on E4000 when unsupported gain value is
    given.

--- rtl.c
+++ rtl.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <rtl-sdr.h>
 #include "dumpvdl2.h"
 #include "rtl.h"
 
 static rtlsdr_dev_t *rtl = NULL;
+
+/* taken from librtlsdr-keenerd, (c) Kyle Keen */
+static int nearest_gain(rtlsdr_dev_t *dev, int target_gain) {
+	int i, r, err1, err2, count, nearest;
+	int* gains;
+	r = rtlsdr_set_tuner_gain_mode(dev, 1);
+	if (r < 0) {
+		fprintf(stderr, "WARNING: Failed to enable manual gain.\n");
+		return r;
+	}
+	count = rtlsdr_get_tuner_gains(dev, NULL);
+	if (count <= 0) {
+		return -1;
+	}
+	gains = XCALLOC(count, sizeof(int));
+	count = rtlsdr_get_tuner_gains(dev, gains);
+	nearest = gains[0];
+	for (i = 0; i < count; i++) {
+		err1 = abs(target_gain - nearest);
+		err2 = abs(target_gain - gains[i]);
+		if (err2 < err1) {
+			nearest = gains[i];
+		}
+	}
+	free(gains);
+	return nearest;
+}
 
 void rtl_init(void *ctx, uint32_t device, int freq, int gain, int correction) {
 	int r;
@@ -39,11 +67,16 @@ void rtl_init(void *ctx, uint32_t device, int freq, int gain, int correction) {
 		} else
 			fprintf(stderr, "Device #%d: gain set to automatic\n", device);
 	} else {
+		int ngain = nearest_gain(rtl, gain);
+		if(ngain < 0) {
+			fprintf(stderr, "Failed to read supported gain list for device #%d: error %d\n", device, ngain);
+			_exit(1);
+		}
 		r = rtlsdr_set_tuner_gain_mode(rtl, 1);
-		r |= rtlsdr_set_tuner_gain(rtl, gain);
+		r |= rtlsdr_set_tuner_gain(rtl, ngain);
 		if (r < 0) {
 			fprintf(stderr, "Failed to set gain to %0.2f for device #%d: error %d\n",
-				(float)gain / 10.0, device, r);
+				(float)ngain / 10.0, device, r);
 			_exit(1);
 		} else
 			fprintf(stderr, "Device #%d: gain set to %0.2f dB\n", device,
