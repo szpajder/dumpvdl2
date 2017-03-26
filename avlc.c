249eab3a6f2cc179620d83d3d34f122c82e00565
Tomasz Lemiech <szpajder@gmail.com>
2017-03-26 09:48:46 +0200

    output_avlc(): Fix time zone name clobbering
    The buffer for strftime() was too short for four-character time zone
    names.

--- avlc.c
+++ avlc.c
@@ -223,7 +223,7 @@ void output_avlc(vdl2_channel_t *v, const avlc_frame_t *f) {
 	if(f == NULL) return;
 	if((daily || hourly) && rotate_outfile() < 0)
 		_exit(1);
-	char ftime[24];
+	char ftime[30];
 	strftime(ftime, sizeof(ftime), "%F %T %Z", (utc ? gmtime(&f->t) : localtime(&f->t)));
 	float sig_pwr_dbfs = 20.0f * log10f(v->mag_frame);
 	float nf_pwr_dbfs = 20.0f * log10f(v->mag_nf + 0.001f);
