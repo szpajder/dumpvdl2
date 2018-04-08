36840f54bbfe77d6a991318b187f756237465005
Tomasz Lemiech <szpajder@gmail.com>
2018-04-08 12:52:24 +0200

    Fix calculation of StatsD processing_time metric
    Starting the timer in DEC_PREAMBLE phase means the result is biased,
    because the frame has not yet been received from the air. We start
    the timer in DEC_DATA phase instead - at this time the requested
    number of bits has been already picked up from the air, so the metric
    will only show the time spent on decoding. This is still slightly
    biased, because the preamble searching and VDL2 header processing
    is not included in the metric now. What's more, the metric includes
    time spent on producing the output, which is not what we want.

--- decode.c
+++ decode.c
@@ -144,9 +144,6 @@ static int deinterleave(uint8_t *in, uint32_t len, uint32_t rows, uint32_t cols,
 void decode_vdl_frame(vdl2_channel_t *v) {
 	switch(v->decoder_state) {
 	case DEC_PREAMBLE:
-#if USE_STATSD
-		gettimeofday(&v->tstart, NULL);
-#endif
 		if(soft_preamble_search(v->bs) == NULL) {
 			statsd_increment(v->freq, "decoder.errors.no_preamble");
 			v->decoder_state = DEC_IDLE;
@@ -201,6 +198,9 @@ void decode_vdl_frame(vdl2_channel_t *v) {
 		v->decoder_state = DEC_DATA;
 		return;
 	case DEC_DATA:
+#if USE_STATSD
+		gettimeofday(&v->tstart, NULL);
+#endif
 		bitstream_descramble(v->bs, &v->lfsr);
 		uint8_t *data = XCALLOC(v->datalen_octets, sizeof(uint8_t));
 		uint8_t *fec = XCALLOC(v->fec_octets, sizeof(uint8_t));
