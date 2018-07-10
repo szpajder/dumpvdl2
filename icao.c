69e6c20edfa18676bfcf2d2f4ab548bd8902059e
Tomasz Lemiech <szpajder@gmail.com>
2018-07-10 21:39:26 +0200

    Suppress raw dumps of ICAO APDUs

--- icao.c
+++ icao.c
@@ -350,9 +350,6 @@ icao_decoding_failed:
 		icao_apdu->data = buf;
 		icao_apdu->datalen = datalen;
 	}
-// temporary, for debugging
-	icao_apdu->raw_data = buf;
-	icao_apdu->datalen = datalen;
 	return icao_apdu;
 }
 
@@ -361,8 +358,6 @@ void output_icao_apdu(icao_apdu_t *icao_apdu) {
 		fprintf(outf, "-- NULL ICAO APDU\n");
 		return;
 	}
-// temporary, for debugging
-	output_raw(icao_apdu->raw_data, icao_apdu->datalen);
 	if(icao_apdu->type != NULL) {
 		if(icao_apdu->data != NULL) {
 			if(dump_asn1)
