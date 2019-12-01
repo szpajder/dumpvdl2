5f966741f73335bfbbfb89c3b37d797215d4ad74
Tomasz Lemiech <szpajder@gmail.com>
2019-12-01 21:22:21 +0100

    Bump version to 1.8.0-pre

--- src/version.cmake
+++ src/version.cmake
@@ -1,12 +1,13 @@
 execute_process(COMMAND git describe --always --tags --dirty
-		OUTPUT_VARIABLE GIT_VERSION
+		OUTPUT_VARIABLE GIT_VERSION_
 		ERROR_QUIET
 		OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-if ("${GIT_VERSION}" STREQUAL "")
+if ("${GIT_VERSION_}" STREQUAL "")
 	set(VERSION "char const * const DUMPVDL2_VERSION=\"${DUMPVDL2_VERSION}\";\n")
 else()
-	set(VERSION "char const * const DUMPVDL2_VERSION=\"${GIT_VERSION}\";\n")
+	string(REGEX REPLACE ".*-g" "" GIT_VERSION ${GIT_VERSION_})
+	set(VERSION "char const * const DUMPVDL2_VERSION=\"${DUMPVDL2_VERSION}-${GIT_VERSION}\";\n")
 endif()
 
 if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/version.c)
