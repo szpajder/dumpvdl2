646b9bbe24b79c7507c5a8681ca702be1fac181c
Tomasz Lemiech <szpajder@gmail.com>
2019-12-30 16:24:55 +0100

    Reformatted usage text

--- src/dumpvdl2.c
+++ src/dumpvdl2.c
@@ -149,87 +149,104 @@ void process_file(vdl2_state_t *ctx, char *path, enum sample_formats sfmt) {
 }
 
 void usage() {
-	fprintf(stderr, "Usage:\n\n");
+	fprintf(stderr,
+	"Usage:\n\n"
 #ifdef WITH_RTLSDR
-	fprintf(stderr, "RTL-SDR receiver:\n");
-	fprintf(stderr, "\tdumpvdl2 [output_options] --rtlsdr <device_id> [rtlsdr_options] [<freq_1> [freq_2 [...]]]\n");
+	"RTL-SDR receiver:\n"
+	"    dumpvdl2 [output_options] --rtlsdr <device_id> [rtlsdr_options] [<freq_1> [<freq_2> [...]]]\n"
 #endif
 #ifdef WITH_MIRISDR
-	fprintf(stderr, "MIRI-SDR receiver:\n");
-	fprintf(stderr, "\tdumpvdl2 [output_options] --mirisdr <device_id> [mirisdr_options] [<freq_1> [freq_2 [...]]]\n");
+	"MIRI-SDR receiver:\n"
+	"    dumpvdl2 [output_options] --mirisdr <device_id> [mirisdr_options] [<freq_1> [<freq_2> [...]]]\n"
 #endif
 #ifdef WITH_SDRPLAY
-	fprintf(stderr, "SDRPLAY RSP receiver:\n");
-	fprintf(stderr, "\tdumpvdl2 [output_options] --sdrplay <device_id> [sdrplay_options] [<freq_1> [freq_2 [...]]]\n");
+	"SDRPLAY RSP receiver:\n"
+	"    dumpvdl2 [output_options] --sdrplay <device_id> [sdrplay_options] [<freq_1> [<freq_2> [...]]]\n"
 #endif
 #ifdef WITH_SOAPYSDR
-	fprintf(stderr, "SOAPYSDR compatible receiver:\n");
-	fprintf(stderr, "\tdumpvdl2 [output_options] --soapysdr <device_id> [soapysdr_options] [<freq_1> [freq_2 [...]]]\n");
+	"SOAPYSDR compatible receiver:\n"
+	"    dumpvdl2 [output_options] --soapysdr <device_id> [soapysdr_options] [<freq_1> [<freq_2> [...]]]\n"
 #endif
-	fprintf(stderr, "I/Q input from file:\n");
-	fprintf(stderr, "\tdumpvdl2 [output_options] --iq-file <input_file> [file_options] [<freq_1> [freq_2 [...]]]\n");
-	fprintf(stderr, "\ncommon options:\n");
-	fprintf(stderr, "\t<freq_1> [freq_2 [...]]\t\tVDL2 channel frequences, in Hz (max %d simultaneous channels supported).\n", MAX_CHANNELS);
-	fprintf(stderr, "\t\t\t\t\tIf omitted, will use VDL2 Common Signalling Channel (%u Hz)\n", CSC_FREQ);
-	fprintf(stderr, "\noutput_options:\n");
-	fprintf(stderr, "\t--output-file <output_file>\tOutput decoded frames to <output_file> (default: stdout)\n");
-	fprintf(stderr, "\t--hourly\t\t\tRotate output file hourly\n");
-	fprintf(stderr, "\t--daily\t\t\t\tRotate output file daily\n");
-	fprintf(stderr, "\t--utc\t\t\t\tUse UTC timestamps in output and file names\n");
-	fprintf(stderr, "\t--raw-frames\t\t\tOutput AVLC payload as raw bytes\n");
-	fprintf(stderr, "\t--dump-asn1\t\t\tOutput full ASN.1 structure of CM and CPDLC messages\n");
-	fprintf(stderr, "\t--extended-header\t\tOutput additional fields in message header\n");
-	fprintf(stderr, "\t--decode-fragments\t\tDecode higher level protocols in fragmented packets (default: off)\n");
-	fprintf(stderr, "\t--msg-filter <filter_spec>\tMessage types to display (default: all) (\"--msg-filter help\" for details)\n");
-	fprintf(stderr, "\t--output-acars-pp <host:port>\tSend ACARS messages to Planeplotter over UDP/IP\n");
+	"I/Q input from file:\n"
+	"    dumpvdl2 [output_options] --iq-file <input_file> [file_options] [<freq_1> [<freq_2> [...]]]\n"
+	"\n"
+	"common options:\n"
+	"    <freq_1> [<freq_2> [...]]                   VDL2 channel frequences, in Hz (max %d simultaneous channels supported).\n"
+	"                                                If omitted, will use VDL2 Common Signalling Channel (%u Hz)\n",
+	MAX_CHANNELS, CSC_FREQ
+	);
+
+	fprintf(stderr,
+	"\n"
+	"output_options:\n"
+	"    --output-file <output_file>                 Output decoded frames to <output_file> (default: stdout)\n"
+	"    --hourly                                    Rotate output file hourly\n"
+	"    --daily                                     Rotate output file daily\n"
+	"    --utc                                       Use UTC timestamps in output and file names\n"
+	"    --raw-frames                                Output AVLC payload as raw bytes\n"
+	"    --dump-asn1                                 Output full ASN.1 structure of CM and CPDLC messages\n"
+	"    --extended-header                           Output additional fields in message header\n"
+	"    --decode-fragments                          Decode higher level protocols in fragmented packets (default: off)\n"
+	"    --msg-filter <filter_spec>                  Message types to display (default: all) (\"--msg-filter help\" for details)\n"
+	"    --output-acars-pp <host:port>               Send ACARS messages to Planeplotter over UDP/IP\n"
 #ifdef WITH_STATSD
-	fprintf(stderr, "\t--statsd <host>:<port>\t\tSend statistics to Etsy StatsD server <host>:<port> (default: disabled)\n");
+	"    --statsd <host>:<port>                      Send statistics to Etsy StatsD server <host>:<port> (default: disabled)\n"
 #endif
 #ifdef WITH_RTLSDR
-	fprintf(stderr, "\nrtlsdr_options:\n");
-	fprintf(stderr, "\t--rtlsdr <device_id>\t\tUse RTL device with specified ID or serial number (default: ID=0)\n");
-	fprintf(stderr, "\t--gain <gain>\t\t\tSet gain (decibels)\n");
-	fprintf(stderr, "\t--correction <correction>\tSet freq correction (ppm)\n");
-	fprintf(stderr, "\t--centerfreq <center_frequency>\tSet center frequency in Hz (default: auto)\n");
+	"\n"
+	"rtlsdr_options:\n"
+	"    --rtlsdr <device_id>                        Use RTL device with specified ID or serial number (default: ID=0)\n"
+	"    --gain <gain>                               Set gain (decibels)\n"
+	"    --correction <correction>                   Set freq correction (ppm)\n"
+	"    --centerfreq <center_frequency>             Set center frequency in Hz (default: auto)\n"
 #endif
 #ifdef WITH_MIRISDR
-	fprintf(stderr, "\nmirisdr_options:\n");
-	fprintf(stderr, "\t--mirisdr <device_id>\t\tUse Mirics device with specified ID or serial number (default: ID=0)\n");
-	fprintf(stderr, "\t--hw-type <device_type>\t\t0 - default, 1 - SDRPlay\n");
-	fprintf(stderr, "\t--gain <gain>\t\t\tSet gain (in decibels, from 0 to 102 dB)\n");
-	fprintf(stderr, "\t--correction <correction>\tSet freq correction (in Hertz)\n");
-	fprintf(stderr, "\t--centerfreq <center_frequency>\tSet center frequency in Hz (default: auto)\n");
-	fprintf(stderr, "\t--usb-mode <usb_transfer_mode>\t0 - isochronous (default), 1 - bulk\n");
+	"\n"
+	"mirisdr_options:\n"
+	"    --mirisdr <device_id>                       Use Mirics device with specified ID or serial number (default: ID=0)\n"
+	"    --hw-type <device_type>                     0 - default, 1 - SDRPlay\n"
+	"    --gain <gain>                               Set gain (in decibels, from 0 to 102 dB)\n"
+	"    --correction <correction>                   Set freq correction (in Hertz)\n"
+	"    --centerfreq <center_frequency>             Set center frequency in Hz (default: auto)\n"
+	"    --usb-mode <usb_transfer_mode>              0 - isochronous (default), 1 - bulk\n"
 #endif
 #ifdef WITH_SDRPLAY
-	fprintf(stderr, "\nsdrplay_options:\n");
-	fprintf(stderr, "\t--sdrplay <device_id>\t\tUse SDRPlay RSP device with specified ID or serial number (default: ID=0)\n");
-	fprintf(stderr, "\t--gr <gr>\t\t\tSet system gain reduction, in dB, positive (-100 = enables AGC)\n");
-	fprintf(stderr, "\t--agc <set point in dBFS>\tAutomatic Gain Control set point in dBFS, negative (default: 0 = AGC off)\n");
-	fprintf(stderr, "\t--correction <correction>\tSet freq correction (ppm)\n");
-	fprintf(stderr, "\t--centerfreq <center_frequency>\tSet center frequency in Hz (default: auto)\n");
-	fprintf(stderr, "\t--antenna <A/B>\t\t\tRSP2 antenna port selection (default: A)\n");
-	fprintf(stderr, "\t--biast <0/1>\t\t\tRSP2 Bias-T control: 0 - off (default), 1 - on\n");
-	fprintf(stderr, "\t--notch-filter <0/1>\t\tRSP2/1a/duo AM/FM/bcast notch filter control: 0 - off (default), 1 - on\n");
-	fprintf(stderr, "\t--tuner <1/2>\t\t\tRSPduo tuner selection: (default: 1)\n");
+	"\n"
+	"sdrplay_options:\n"
+	"    --sdrplay <device_id>                       Use SDRPlay RSP device with specified ID or serial number (default: ID=0)\n"
+	"    --gr <gr>                                   Set system gain reduction, in dB, positive (-100 = enables AGC)\n"
+	"    --agc <set point in dBFS>                   Automatic Gain Control set point in dBFS, negative (default: 0 = AGC off)\n"
+	"    --correction <correction>                   Set freq correction (ppm)\n"
+	"    --centerfreq <center_frequency>             Set center frequency in Hz (default: auto)\n"
+	"    --antenna <A/B>                             RSP2 antenna port selection (default: A)\n"
+	"    --biast <0/1>                               RSP2 Bias-T control: 0 - off (default), 1 - on\n"
+	"    --notch-filter <0/1>                        RSP2/1a/duo AM/FM/bcast notch filter control: 0 - off (default), 1 - on\n"
+	"    --tuner <1/2>                               RSPduo tuner selection: (default: 1)\n"
 #endif
 #ifdef WITH_SOAPYSDR
-	fprintf(stderr, "\nsoapysdr_options:\n");
-	fprintf(stderr, "\t--soapysdr <device_id>\t\tUse SoapySDR compatible device with specified ID (default: ID=0)\n");
-	fprintf(stderr, "\t--device-settings <key1=val1,key2=val2,...>\tSet device-specific parameters (default: none)\n");
-	fprintf(stderr, "\t--gain <gain>\t\t\tSet gain (decibels)\n");
-	fprintf(stderr, "\t--correction <correction>\tSet freq correction (ppm)\n");
-	fprintf(stderr, "\t--soapy-antenna <antenna>\tSet antenna port selection (default: RX)\n");
-	fprintf(stderr, "\t--soapy-gain <gain1=val1,gain2=val2,...>\tSet gain components (default: none)\n");
+	"\n"
+	"soapysdr_options:\n"
+	"    --soapysdr <device_id>                      Use SoapySDR compatible device with specified ID (default: ID=0)\n"
+	"    --device-settings <key1=val1,key2=val2,...> Set device-specific parameters (default: none)\n"
+	"    --gain <gain>                               Set gain (decibels)\n"
+	"    --correction <correction>                   Set freq correction (ppm)\n"
+	"    --soapy-antenna <antenna>                   Set antenna port selection (default: RX)\n"
+	"    --soapy-gain <gain1=val1,gain2=val2,...>    Set gain components (default: none)\n"
 #endif
-	fprintf(stderr, "\nfile_options:\n");
-	fprintf(stderr, "\t--iq-file <input_file>\t\tRead I/Q samples from file\n");
-	fprintf(stderr, "\t--centerfreq <center_frequency>\tCenter frequency of the input data, in Hz (default: 0)\n");
-	fprintf(stderr, "\t--oversample <oversample_rate>\tOversampling rate for recorded data (default: %u)\n", FILE_OVERSAMPLE);
-	fprintf(stderr, "\t\t\t\t\t  (sampling rate will be set to %u * oversample_rate)\n", SYMBOL_RATE * SPS);
-	fprintf(stderr, "\t--sample-format <sample_format>\tInput sample format. Supported formats:\n");
-	fprintf(stderr, "\t\t\t\t\t  U8\t\t8-bit unsigned (eg. recorded with rtl_sdr) (default)\n");
-	fprintf(stderr, "\t\t\t\t\t  S16_LE\t16-bit signed, little-endian (eg. recorded with miri_sdr)\n");
+	"\n"
+	"file_options:\n"
+	"    --iq-file <input_file>                      Read I/Q samples from file\n"
+	"    --centerfreq <center_frequency>             Center frequency of the input data, in Hz (default: 0)\n"
+	"    --oversample <oversample_rate>              Oversampling rate for recorded data (default: %u)\n"
+	"                                                (sampling rate will be set to %u * oversample_rate)\n",
+	FILE_OVERSAMPLE, SYMBOL_RATE * SPS
+	);
+
+	fprintf(stderr,
+	"    --sample-format <sample_format>             Input sample format. Supported formats:\n"
+	"                                                    U8     8-bit unsigned (eg. recorded with rtl_sdr) (default)\n"
+	"                                                    S16_LE 16-bit signed, little-endian (eg. recorded with miri_sdr)\n"
+	);
 	_exit(0);
 }
 
