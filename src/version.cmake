22bfb816330ee93b65b241386544e193621ed8d6
Tomasz Lemiech <szpajder@gmail.com>
2020-01-23 22:37:18 +0100

    version.cmake: fix output path + declutter a bit

--- src/version.cmake
+++ src/version.cmake
@@ -1,24 +1,26 @@
+set (VERSION "char const * const DUMPVDL2_VERSION=\"")
 execute_process(COMMAND git describe --always --tags --dirty
 		OUTPUT_VARIABLE GIT_VERSION
 		ERROR_QUIET
 		OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 if ("${GIT_VERSION}" STREQUAL "")
-	set(VERSION "char const * const DUMPVDL2_VERSION=\"${DUMPVDL2_VERSION}\";\n")
+	string(APPEND VERSION "${DUMPVDL2_VERSION}")
 elseif("${GIT_VERSION}" MATCHES ".+-g(.+)")
-	set(VERSION "char const * const DUMPVDL2_VERSION=\"${DUMPVDL2_VERSION}-${CMAKE_MATCH_1}\";\n")
+	string(APPEND VERSION "${DUMPVDL2_VERSION}-${CMAKE_MATCH_1}")
 elseif("${GIT_VERSION}" MATCHES "v(.+)")
-	set(VERSION "char const * const DUMPVDL2_VERSION=\"${CMAKE_MATCH_1}\";\n")
+	string(APPEND VERSION "${CMAKE_MATCH_1}")
 else()
-	set(VERSION "char const * const DUMPVDL2_VERSION=\"${DUMPVDL2_VERSION}-${GIT_VERSION}\";\n")
+	string(APPEND VERSION "${DUMPVDL2_VERSION}-${GIT_VERSION}")
 endif()
+string(APPEND VERSION "\";\n")
 
-if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/version.c)
-	file(READ ${CMAKE_CURRENT_SOURCE_DIR}/version.c VERSION_)
+if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/version.c)
+	file(READ ${CMAKE_CURRENT_BINARY_DIR}/version.c VERSION_)
 else()
 	set(VERSION_ "")
 endif()
 
 if (NOT "${VERSION}" STREQUAL "${VERSION_}")
-	file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/version.c "${VERSION}")
+	file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/version.c "${VERSION}")
 endif()
