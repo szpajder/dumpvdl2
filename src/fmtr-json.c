459bbeae0d61f34191bbdd8604ba9365816536fb
Kevin Elliott <kevin@welikeinc.com>
2020-08-24 21:30:36 -0700

    Add the version to the json output

--- src/fmtr-json.c
+++ src/fmtr-json.c
@@ -56,8 +56,8 @@ static octet_string_t *fmtr_json_format_decoded_msg(vdl2_msg_metadata *metadata,
 	la_vstring *vstrAcars = acars_format_json(root);
 
 	la_vstring_append_sprintf(vstr,
-      "{ \"source_app\": \"dumpvdl2\", \"timestamp\": \"%s\", \"frequency\": \"%.3f\", \"frame_pwr_dbfs\": \"%.1f\", \"nf_pwr_dbfs\": \"%.1f\", \"ppm_error\": \"%.1f\", ",
-			timestamp->str, (float)metadata->freq / 1e+6, metadata->frame_pwr_dbfs, metadata->nf_pwr_dbfs,
+      "{ \"source_app\": \"dumpvdl2\", \"source_app_version\", \"%s\", \"timestamp\": \"%s\", \"frequency\": \"%.3f\", \"frame_pwr_dbfs\": \"%.1f\", \"nf_pwr_dbfs\": \"%.1f\", \"ppm_error\": \"%.1f\", ",
+			DUMPVDL2_VERSION, timestamp->str, (float)metadata->freq / 1e+6, metadata->frame_pwr_dbfs, metadata->nf_pwr_dbfs,
       metadata->ppm_error);
 	la_vstring_destroy(timestamp, true);
 
