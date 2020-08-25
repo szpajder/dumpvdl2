a572896567d445a5aac91f19a0f37fb1017b7c2b
Kevin Elliott <kevin@welikeinc.com>
2020-08-24 21:28:39 -0700

    Nope, did not need the null terminator length

--- src/fmtr-json.c
+++ src/fmtr-json.c
@@ -73,7 +73,7 @@ static octet_string_t *fmtr_json_format_decoded_msg(vdl2_msg_metadata *metadata,
 	EOL(vstr);
 
 	// vstr = la_proto_tree_format_text(vstr, root);
-	octet_string_t *ret = octet_string_new(vstr->str, vstr->len + 1);     // +1 for NULL terminator
+	octet_string_t *ret = octet_string_new(vstr->str, vstr->len);
 	la_vstring_destroy(vstr, false);
 	return ret;
 }
