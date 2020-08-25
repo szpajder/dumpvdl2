e5f03f9ac8be6b4b9ae6c3e2608c6f09cc41cb29
Kevin Elliott <kevin@welikeinc.com>
2020-08-24 22:00:52 -0700

    Testing something

--- src/fmtr-json.c
+++ src/fmtr-json.c
@@ -72,9 +72,8 @@ static octet_string_t *fmtr_json_format_decoded_msg(vdl2_msg_metadata *metadata,
         "\"acars\": %s }",
         vstrAcars->str);
   } else {
-    la_vstring_append_sprintf(vstr, " }");
+    la_vstring_append_sprintf(vstr, " } ");
   }
-
 
 	// vstr = la_proto_tree_format_text(vstr, root);
 	octet_string_t *ret = octet_string_new(vstr->str, vstr->len);
